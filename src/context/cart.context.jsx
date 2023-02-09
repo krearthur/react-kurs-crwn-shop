892fe60ea0b1a44142bd75a6878464ddcfe079c0
Grohe, Arthur <arthur.grohe@greenique.de>
2023-02-09 18:21:13 +0100

    added in the addToCart feature

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,15 +1,51 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingItem = cartItems.find((item) => item.id === productToAdd.id);
+  if (existingItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  } else {
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+  }
+};
 
 export const CartContext = createContext({
-  products: [],
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: () => {},
 });
 
 export const CartProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { products, setProducts, isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const count = cartItems.reduce(
+      (subTotal, cartItem) => subTotal + cartItem.quantity,
+      0
+    );
+    setCartCount(count);
+  }, [cartItems]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = {
+    cartItems,
+    addItemToCart,
+    isCartOpen,
+    setIsCartOpen,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
